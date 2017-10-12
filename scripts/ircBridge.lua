81ce264d863a8c2ba665aa18d90a18315ee16227
Michael Fitzmayer <mail@michael-fitzmayer.de>
2017-10-12 21:03:41 +0200

    Emacs is love, Emacs is life.

--- scripts/ircBridge.lua
+++ scripts/ircBridge.lua
@@ -1,3 +1,4 @@
+-- ircBridge.lua -*-lua-*-
 -- "THE BEER-WARE LICENSE" (Revision 42):
 -- <mail@michael-fitzmayer.de> wrote this file.  As long as you retain
 -- this notice you can do whatever you want with this stuff. If we meet
