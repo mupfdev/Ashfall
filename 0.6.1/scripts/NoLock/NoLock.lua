8b37ddcdc071d13267b8bcaec7f8cacea70e9bc2
1338 <maxvandersluis@yahoo.com>
2017-11-14 19:18:18 +0100

    Added the NoLock script made by Kibiri-daro.

---
+++ 0.6.1/scripts/NoLock/NoLock.lua
@@ -0,0 +1,24 @@
+NoLock = {}
+
+NoLock.OnObjectLock = function(pid, cellDescription)
+    local lcell = LoadedCells[cellDescription]
+    tes3mp.ReadLastEvent()
+    print("Checking if this actually triggers")
+    for i = 0, tes3mp.GetObjectChangesSize() - 1 do
+        local refIndex = tes3mp.GetObjectRefNumIndex(i) .. "-" .. tes3mp.GetObjectMpNum(i)
+        local refId = tes3mp.GetObjectRefId(i)
+        local lockLevel = tes3mp.GetObjectLockLevel(i)
+
+        --Do a check to see if this is something you don't want to lock
+        if true then
+            --We assume this is being done after myMod saves the lock data, so we don't have to check anything's valid
+            lcell.data.objectData[refIndex].lockLevel = 0
+            for pid, player in pairs(Players) do
+                lcell:SendObjectsLocked(pid)
+            end
+        end
+    end
+end
+
+return NoLock
+
---
+++ 0.6.1/scripts/NoLock/README.md
@@ -0,0 +1,26 @@
+# NoLock script
+
+## Use case
+When a door is locked this script unlocks it. This is to prevent griefing.
+
+## NoLock instructions
+
+Add this at the top of server.lua:
+` NoLock = require("NoLock") `
+
+Then find:
+  `function OnObjectLock(pid, cellDescription)
+    myMod.OnObjectLock(pid, cellDescription)
+   end `
+
+add this to it:
+  ` noLock.OnObjectLock(pid, cellDescription) `
+
+Now it should look like:
+  ` function OnObjectLock(pid, cellDescription)
+      myMod.OnObjectLock(pid, cellDescription)
+      noLock.OnObjectLock(pid, cellDescription)
+    end `
+
+## Credits
+Credits fully go to: Atkana#0128 or Kibiri-daro
