9641613bae3754b505010114db5a93445c9ba53f
Michael Fitzmayer <mail@michael-fitzmayer.de>
2017-11-19 16:22:25 +0100

    Fixed typo.

--- misc/ashfall/bin/cleanup_daily.sh
+++ misc/ashfall/bin/cleanup_daily.sh
@@ -3,7 +3,7 @@
 day=`date --date='-1 day' +'%Y-%m-%d'`
 
 mkdir ~/backups/player/${day}
-mv ~/backups/player/${day}_* ~backups/player/${day}/
+mv ~/backups/player/${day}_* ~/backups/player/${day}/
 
 find ~/0.6.1-server/keepers/CoreScripts/data/player \
 	 -maxdepth 1 -mtime +7 -exec \
