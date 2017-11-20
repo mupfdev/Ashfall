5d4cb173c18a34fc5e0c5f422892e74b8fa0a56b
Michael Fitzmayer <mail@michael-fitzmayer.de>
2017-11-20 22:33:14 +0100

    Added placeholder for 'VirtualSeptims'.

---
+++ mods/VirtualSeptims/main.lua
@@ -0,0 +1,15 @@
+-- TES3MP VirtualSeptims -*-lua-*-
+-- "THE BEER-WARE LICENSE" (Revision 42):
+-- <mail@michael-fitzmayer.de> wrote this file.  As long as you retain
+-- this notice you can do whatever you want with this stuff. If we meet
+-- some day, and you think this stuff is worth it, you can buy me a beer
+-- in return.  Michael Fitzmayer
+
+
+require("color")
+JsonInterface = require("jsonInterface")
+
+local storage = JsonInterface.load(getDataFolder() .. "storage.json")
+
+
+-- Placeholder
