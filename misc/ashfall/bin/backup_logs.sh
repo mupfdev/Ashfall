1b96d9c6e7d16a0a552a2b5017d40e2e4386c2dc
Michael Fitzmayer <mail@michael-fitzmayer.de>
2017-11-22 09:34:48 +0100

    Typo.

--- misc/ashfall/bin/backup_logs.sh
+++ misc/ashfall/bin/backup_logs.sh
@@ -4,7 +4,7 @@ today=`date '+%Y-%m-%d_%H:%M:%S'`
 
 mkdir ~/backups/logs/${today}
 ls --format=single-column --sort time \
-   ~/0.6.1-server/keepers/home/config/openmw/*.log > ~/bin/tmpFiles.txt
+   ~/0.6.1-server/keepers/home/config/*.log > ~/bin/tmpFiles.txt
 
 sed -i '1d' ~/bin/tmpFiles.txt
 xargs -a ~/bin/tmpFiles.txt mv -t ~/backups/logs/${today}
