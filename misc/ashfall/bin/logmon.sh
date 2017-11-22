e82b785c0426162d81bb7117aa6717ca9bf2c659
Michael Fitzmayer <mail@michael-fitzmayer.de>
2017-11-22 09:49:23 +0100

    Fixed 'logmon.sh'.

--- misc/ashfall/bin/logmon.sh
+++ misc/ashfall/bin/logmon.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-LOG=`ls ~/backups/logs/current/ | awk -F_ '{print $1 $2}' | sort -n -k 2,2 | tail -1`
+LOG=`ls --format=single-column ~/backups/logs/current/ | sort -n -t _ -k 2 | tail -1`
 
 tail -f ~/backups/logs/current/$LOG
