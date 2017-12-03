9e1ffff4b785a753a271f4b26b93f0821d129822
Atkana <Em0srawk@live.com>
2017-12-03 18:03:40 +0000

    Update DeathDrop.lua
    Maybe making it work (untested :P)

--- 0.6.1/scripts/DeathDrop/DeathDrop.lua
+++ 0.6.1/scripts/DeathDrop/DeathDrop.lua
@@ -15,45 +15,45 @@ Methods = {}
 -- [ DeathDrop.Drop(pid) ]
 -- directly above it.
 
+Methods.Drop = function(pid)	
+	for index, item in pairs(Players[pid].data.inventory) do
+	
+		local mpNum = WorldInstance:GetCurrentMpNum() + 1
+		local cell = tes3mp.GetCell(pid)
+		local location = {
+			posX = tes3mp.GetPosX(pid), posY = tes3mp.GetPosY(pid), posZ = tes3mp.GetPosZ(pid),
+		}
+		local refId = item.refId
+		local refIndex =  0 .. "-" .. mpNum
+		
+		WorldInstance:SetCurrentMpNum(mpNum)
+		tes3mp.SetCurrentMpNum(mpNum)
 
-Methods.Drop = function(pid)
-    if Players[pid] ~= nil and Players[pid]:IsLoggedIn() then
-        for index, item in pairs (Players[pid].data.inventory) do
+		LoadedCells[cell]:InitializeObjectData(refIndex, refId)
+		LoadedCells[cell].data.objectData[refIndex].location = location
+		table.insert(LoadedCells[cell].data.packets.place, refIndex)
+		LoadedCells[cell]:Save()
+		
+		for onlinePid, player in pairs(Players) do
+			if player:IsLoggedIn() then
+				tes3mp.InitializeEvent(onlinePid)
+				tes3mp.SetEventCell(cell)
+				tes3mp.SetObjectRefId(refId)
+				tes3mp.SetObjectRefNumIndex(0)
+				tes3mp.SetObjectMpNum(mpNum)
 
-            local mpNum = WorldInstance:GetCurrentMpNum() + 1
-            local cell = tes3mp.GetCell(pid)
-            local location = {
-                posX = tes3mp.GetPosX(pid), posY = tes3mp.GetPosY(pid), posZ = tes3mp.GetPosZ(pid),
-            }
-            local refId = item.refId
-            local refIndex =  0 .. "-" .. mpNum
-
-            WorldInstance:SetCurrentMpNum(mpNum)
-            tes3mp.SetCurrentMpNum(mpNum)
-
-            LoadedCells[cell]:InitializeObjectData(refIndex, refId)
-            LoadedCells[cell].data.objectData[refIndex].location = location
-            table.insert(LoadedCells[cell].data.packets.spawn, refIndex)
-            table.insert(LoadedCells[cell].data.packets.actorList, refIndex)
-            LoadedCells[cell]:Save()
-
-            tes3mp.InitializeEvent(pid)
-            tes3mp.SetEventCell(cell)
-            tes3mp.SetObjectRefId(refId)
-            tes3mp.SetObjectRefNumIndex(0)
-            tes3mp.SetObjectMpNum(mpNum)
-
-            tes3mp.SetObjectPosition(location.posX, location.posY, location.posZ)
-            tes3mp.AddWorldObject()
-            tes3mp.SendObjectPlace()
-
-            Players[pid].data.inventory[index] = nil
-        end
-
-        Players[pid]:Save()
-        Players[pid]:LoadInventory()
-    end
+				tes3mp.SetObjectPosition(location.posX, location.posY, location.posZ)
+				tes3mp.AddWorldObject()
+				tes3mp.SendObjectPlace()
+			end
+		end
+		
+		Players[pid].data.inventory[index] = nil
+	end
+	
+	Players[pid]:Save()
+    Players[pid]:LoadInventory()
+	Players[pid]:LoadEquipment()
 end
-
 
 return Methods
